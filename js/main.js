167a1eecda576b3328e7ff27c19ff36310247f72
Rubical <zhpiv96@mail.ru>
2022-12-04 21:34:36 +0300

    added what-we-do nav board

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
+
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle('dark-mode-btn--active');
     document.body.classList.toggle('dark');
-}
+};
 
-function scrollNav() {
-    $('.nav a').click(function(){
-      $(".active").removeClass("active");     
-      $(this).addClass("active");
-      
-      $('html, body').stop().animate({
-        scrollTop: $($(this).attr('href')).offset().top - 160
-      }, 300);
-      return false;
-    });
-  }
-  scrollNav();
+
