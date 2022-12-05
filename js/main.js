86c691da63b0baacc1dc8c77962fc650cb33c7b4
Rubical <zhpiv96@mail.ru>
2022-12-05 22:07:11 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
-
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle('dark-mode-btn--active');
     document.body.classList.toggle('dark');
