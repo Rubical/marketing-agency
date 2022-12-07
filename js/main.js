36962f51890f2fe487451631705b8cd2e547c071
Rubical <zhpiv96@mail.ru>
2022-12-07 22:29:50 +0300

    connected modal box with buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
+// Dark mode button functional
+
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle('dark-mode-btn--active');
     document.body.classList.toggle('dark');
 };
+
+// Modal box functional
+
+const modalBtns = document.querySelectorAll(".modal-box--active");
+const modalOverlay = document.querySelector(".modal-box-overlay");
+const modalCloseBtn = document.querySelector(".modal-box-close");
+
+modalBtns.forEach((el) => {
+el.addEventListener('click', (e) => {
+    modalOverlay.classList.add('modal-box-overlay--visible');
+    })
+});
+
+modalCloseBtn.addEventListener('click', (e) => {
+    modalOverlay.classList.remove('modal-box-overlay--visible');
+    });
